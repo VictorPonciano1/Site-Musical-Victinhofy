50556e0cc03ab2d43241e4f264a19eff01c45021
Victor Ponciano <victordias.ponciano@gmail.com>
2025-01-30 11:54:17 -0300

    projeto com apenas uma musica

---
+++ script.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+
+    /*
+
+    //MÚSICA 1
+    var audio = $("#audio1")[0];
+    $("#play1").click(function(){
+        audio.play();
+    });
+    $("#pause1").click(function(){
+        audio.pause();
+    });
+    $("#stop1").click.click(function(){
+        audio.pause();
+        audio.currentTime = 0;
+    });
+
+    //MÚSICA 2
+    var audio = $("#audio2")[0];
+    $("#play2").click(function(){
+        audio.play();
+    });
+    $("#pause2").click(function(){
+        audio.pause();
+    });
+    $("#stop2").click.click(function(){
+        audio.pause();
+        audio.currentTime = 0;
+    });
+
+    //MÚSICA 3
+    var audio = $("#audio3")[0];
+    $("#play3").click(function(){
+        audio.play();
+    });
+    $("#pause3").click(function(){
+        audio.pause();
+    });
+    $("#stop3").click.click(function(){
+        audio.pause();
+        audio.currentTime = 0;
+    });
+
+    */
+
+    //SCRIPT DA MÚSICA SECRETA
+    var audio = $("#audioSecreto")[0];
+    $("#play").click(function(){
+        audio.play();
+    });
+    $("#pause").click(function(){
+        audio.pause();
+    });
+    $("#stop").click.click(function(){
+        audio.pause();
+        audio.currentTime = 0;
+    });
+
+});
